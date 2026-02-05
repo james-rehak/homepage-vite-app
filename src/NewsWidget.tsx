cd6bf1dcf91553f5eb9dace424354692b6875b82
James <jmba3>
2026-02-05 05:58:45 -0500

    1- Add news widget component

---
+++ src/NewsWidget.tsx
@@ -0,0 +1,14 @@
+/**
+ * Todo Cache Results
+ * */
+import React from "react";
+
+export default function NewsWidget() {
+  return (
+    <div className="p-6 bg-white border rounded-xl shadow-sm hover:shadow-md transition-all">
+      <h3 className="font-bold text-blue-600">News Widget</h3>
+      <div className="flex items-center mt-4 gap-2">
+      </div>
+    </div>
+  );
+};
