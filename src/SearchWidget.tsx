91d723410545cf085d6ed27a1cccef61a9401df3
James <jmba3>
2026-02-04 20:03:43 -0500

    adjust styling

--- src/SearchWidget.tsx
+++ src/SearchWidget.tsx
@@ -5,7 +5,7 @@
  * */
 import React, {useState} from "react";
 
-const url = "https://searxng.site/search?q=";
+const url = "https://searx.party/search?q=";
 
 export default function SearchWidget() {
   const [query, setQuery] = useState("");
@@ -27,20 +27,23 @@ export default function SearchWidget() {
   return (
     <div className="p-6 bg-white border rounded-xl shadow-sm hover:shadow-md transition-all">
       <h3 className="font-bold text-blue-600">Search Widget</h3>
-      <input
-        id="search"
-        type="text"
-        placeholder="Search..."
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        className="text-black mt-4 w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-blue-600"
-        onClick={submitQuery}
-      >
-        Search
-      </button>
+      <div className="flex items-center mt-4 gap-2">
+        <input
+          id="search"
+          type="text"
+          placeholder="Search..."
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          className="text-black w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          onClick={submitQuery}
+          className="bg-amber-600 rounded-md hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          &#x1F50D;
+        </button>
+      </div>
     </div>
   );
+
 }
